bfdd5174b35d5da1b77359ea4264150ec6a45cf9
[dyad] <yea13sin@gmail.com>
2025-08-26 03:45:36 +0600

    [dyad] Fixed main layout rendering by using React Router's Outlet - wrote 1 file(s)

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,16 @@
 "use client";
 
 import React from 'react';
+import { Outlet } from 'react-router-dom'; // Import Outlet
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = ({ children }) => {
+const Layout = () => { // No need for 'children' prop when using Outlet
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow container mx-auto p-4">
-        {children}
+        <Outlet /> {/* Render the nested route components here */}
       </main>
       <Footer />
     </div>
