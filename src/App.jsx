3281d654fcc76431bc2170f02b5590578eb56900
[dyad] <yea13sin@gmail.com>
2025-08-26 03:58:30 +0600

    [dyad] Changed toast messages to light theme - wrote 1 file(s)

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ const App = () => (
     <TooltipProvider>
       <>
         <Toaster />
-        <Sonner position="top-center" /> {/* Added position="top-center" here */}
+        <Sonner position="top-center" theme="light" /> {/* Added theme="light" here */}
         <BrowserRouter>
           <SessionContextProvider>
             <Routes>
