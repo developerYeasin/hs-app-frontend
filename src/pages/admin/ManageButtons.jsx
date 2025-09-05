3d695bae2e4059a696283ec052a59008c3d97e0a
[dyad] <yea13sin@gmail.com>
2025-09-06 02:59:38 +0600

    [dyad] Fixed delete button modal display and centering on /admin/buttons. - wrote 1 file(s)

--- src/pages/admin/ManageButtons.jsx
+++ src/pages/admin/ManageButtons.jsx
@@ -186,7 +186,7 @@ const ManageButtons = () => {
 
       {/* Global AlertDialog for delete confirmation */}
       <AlertDialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
-        <AlertDialogContent>
+        <AlertDialogContent className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full">
           <AlertDialogHeader>
             <AlertDialogTitle>
               Are you absolutely sure?
@@ -226,7 +226,7 @@ const ManageButtons = () => {
       {selectedButton && (
         <ViewButtonModal
           isOpen={isViewModalOpen}
-          onOpenChange={setIsViewModalChange} // Corrected prop name
+          onOpenChange={setIsViewModalOpen} {/* Corrected prop name here */}
           button={selectedButton}
         />
       )}
